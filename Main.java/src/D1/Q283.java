f5c2f0cd7f9ce0fda0b4fa95d9dca5e1a1b41d4f
xiachen97 <122903592+xiachen97@users.noreply.github.com>
2024-01-10 23:41:41 -0500

    Update Q283.java

--- Main.java/src/D1/Q283.java
+++ Main.java/src/D1/Q283.java
@@ -14,6 +14,12 @@ public class Q283 {
             System.out.print(k);//1 2 3 0 0
         }
 
+        int[] n={0,1,0,3,2};
+        moveZeroForward(n);
+        for(int x:n){
+            System.out.print(x);//0,0,1,3,2
+        }
+
     }
     public static void moveZeroes(int[] nums) {
         int i=0;
@@ -26,5 +32,19 @@ public class Q283 {
         for(int k=i;k<nums.length;k++){
             nums[k]=0;
         }
+    }
+    public static void moveZeroForward(int[] nums){
+       int i=0;
+       for(int j=1;j<nums.length;j++){
+           if(nums[i]!=0&&nums[j]==0){
+               //1. swap nums[i] and nums[j]  //2. or directly let nums[i]=nums[j]; nums[j]=0;
+               int temp=nums[i];
+               nums[i]=nums[j];
+               nums[j]=temp;
+           }
+           i++;
+       }
+
+        
     }
 }
