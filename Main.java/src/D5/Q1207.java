5d99efec4c9a12b454de4f4e9293aad51cffe3d1
xiachen97 <xiachen2@oakland.edu>
2024-01-12 13:50:10 -0500

    D5

---
+++ Main.java/src/D5/Q1207.java
@@ -0,0 +1,28 @@
+package D5;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Q1207 {
+    public static void main(String[] args) {
+        int[] arr={1,2,2,1,1,3,2};
+        System.out.println(uniqueOccurrences(arr));
+    }
+    public static boolean uniqueOccurrences(int[] arr) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            if(map.containsKey(arr[i])){
+                map.put(arr[i],map.get(arr[i])+1);
+            }
+            else{
+                map.put(arr[i],1);
+            }
+           
+        }
+        //System.out.println(map);
+
+        //check if the set each num are unique
+        HashSet<Integer> set=new HashSet<>(map.values());
+        return  set.size()==map.size();
+    }
+}
