696e17536833221f4eb2a159fe814e5a1027ff52
xiachen97 <xiachen2@oakland.edu>
2024-01-13 17:15:39 -0500

    D6

--- Main.java/src/D6/Q1657.java
+++ Main.java/src/D6/Q1657.java
@@ -8,8 +8,8 @@ import java.util.HashSet;
 
 public class Q1657 {
     public static void main(String[] args) {
-        String word1 = "abc";
-        String  word2 = "bac";
+        String word1 = "abcaa";
+        String  word2 = "bacbb";
         System.out.println(closeStrings(word1, word2));
     }
     public static boolean closeStrings(String word1, String word2) {
@@ -48,14 +48,21 @@ public class Q1657 {
 
 
         //2.
-        if(word1.length() != word2.length()) 
-            return false;
-
-        int arr1[] = new int[26], arr2[] = new int[26];
+        if(word1.length() != word2.length()) {return false;}
+        //calculate every character(26) occurrence times
+        int[] arr1=new int[26];
+        int[] arr2=new int[26];
         HashSet<Integer> set = new HashSet<>();
 
-        ...
-
+        for(char c:word1.toCharArray()){
+            arr1[c-'a']++; //Count the number of occurrences of a specific character in character array arr1
+            set.add(c-'a');//[0,1,2]=>a,b,c
+        }
+        for(char c:word2.toCharArray()){
+            if(set.contains(c-'a')){
+                arr2[c-'a']++;
+            }
+        }
         Arrays.sort(arr1);
         Arrays.sort(arr2);
         return Arrays.equals(arr1, arr2);
